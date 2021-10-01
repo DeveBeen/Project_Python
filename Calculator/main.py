b4d265f9582d62ea74397b3fb843fb16ee6234e1
Kimwonbin <pransis1@naver.com>
2021-10-02 00:15:50 +0900

    Basic - calculator

---
+++ Calculator/main.py
@@ -0,0 +1,28 @@
+# 계산기 프로그램
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x , y):
+    return x * y
+
+def devide(x, y):
+    return x / y
+
+first_number = int(input('Input First Number :'))
+calculator = input('Select +, -, *, / :')
+second_number = int(input('Input Second Number :'))
+
+if calculator == '+':
+    print(add(first_number, second_number))
+elif calculator == '-':
+    print(subtract(first_number, second_number))
+elif calculator == '*':
+    print(multiply(first_number, second_number))
+elif calculator == '/':
+    print(devide(first_number, second_number))
+else:
+    print('Error')
