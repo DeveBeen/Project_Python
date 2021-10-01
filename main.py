9be0dff1e21e85abf2477adf4b171c89165241e7
Kimwonbin <pransis1@naver.com>
2021-10-01 22:30:42 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Python Vendingmachine
+
+print('Wellcome Kim\'s Vending Machine')
+
+def First_Setting(Product_number, **Product_data):
+
+    print('Please input your Vending Machine data')
+
+    Product_number = int(input('Enter product number in your vending machine'))
+
+    for i range(0,Product_number):
+        Product_data = input()
+
+
+User_vaendingmachine = input('Enter, You want a product')
