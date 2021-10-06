b8da95ce67ad8b3ca649db1d3fbaa3a0ade785a3
Kimwonbin <pransis1@naver.com>
2021-10-06 17:23:09 +0900

    Incomplete - VendingMachine

---
+++ VendingMachine/drink_class.py
@@ -0,0 +1,221 @@
+# 각각의 음료수 개체
+
+class a_drink:
+    drink_num = 0
+
+    def __init__(self, stocknum):
+        self.stocknum = stocknum
+        a_drink.drink_num += 1
+
+    def __del__(self):
+        a_drink.drink_num -= 1
+
+#-------------------------------------------------------------------------------
+class b_drink:
+    drink_num = 0
+
+    def __init__(self, stocknum):
+        self.stocknum = stocknum
+        b_drink.drink_num += 1
+
+    def __del__(self):
+        b_drink.drink_num -= 1
+
+#-------------------------------------------------------------------------------
+class c_drink:
+    drink_num = 0
+
+    def __init__(self, stocknum):
+        self.stocknum = stocknum
+        c_drink.drink_num += 1
+
+    def __del__(self):
+        c_drink.drink_num -= 1
+
+#-------------------------------------------------------------------------------
+class d_drink:
+    drink_num = 0
+
+    def __init__(self, stocknum):
+        self.stocknum = stocknum
+        d_drink.drink_num += 1
+
+    def __del__(self):
+        d_drink.drink_num -= 1
+
+#-------------------------------------------------------------------------------
+class e_drink:
+    drink_num = 0
+
+    def __init__(self, stocknum):
+        self.stocknum = stocknum
+        e_drink.drink_num += 1
+
+    def __del__(self):
+        e_drink.drink_num -= 1
+
+#-------------------------------------------------------------------------------
+class f_drink:
+    drink_num = 0
+
+    def __init__(self, stocknum):
+        self.stocknum = stocknum
+        f_drink.drink_num += 1
+
+    def __del__(self):
+        f_drink.drink_num -= 1
+
+#-------------------------------------------------------------------------------
+class g_drink:
+    drink_num = 0
+
+    def __init__(self, stocknum):
+        self.stocknum = stocknum
+        g_drink.drink_num += 1
+
+    def __del__(self):
+        g_drink.drink_num -= 1
+
+#-------------------------------------------------------------------------------
+class h_drink:
+    drink_num = 0
+
+    def __init__(self, stocknum):
+        self.stocknum = stocknum
+        h_drink.drink_num += 1
+
+    def __del__(self):
+        h_drink.drink_num -= 1
+
+#-------------------------------------------------------------------------------
+class i_drink:
+    drink_num = 0
+
+    def __init__(self, stocknum):
+        self.stocknum = stocknum
+        i_drink.drink_num += 1
+
+    def __del__(self):
+        i_drink.drink_num -= 1
+
+#-------------------------------------------------------------------------------
+class j_drink:
+    drink_num = 0
+
+    def __init__(self, stocknum):
+        self.stocknum = stocknum
+        j_drink.drink_num += 1
+
+    def __del__(self):
+        j_drink.drink_num -= 1
+
+#-------------------------------------------------------------------------------
+class k_drink:
+    drink_num = 0
+
+    def __init__(self, stocknum):
+        self.stocknum = stocknum
+        k_drink.drink_num += 1
+
+    def __del__(self):
+        k_drink.drink_num -= 1
+
+#-------------------------------------------------------------------------------
+class l_drink:
+    drink_num = 0
+
+    def __init__(self, stocknum):
+        self.stocknum = stocknum
+        l_drink.drink_num += 1
+
+    def __del__(self):
+        l_drink.drink_num -= 1
+
+#-------------------------------------------------------------------------------
+class m_drink:
+    drink_num = 0
+
+    def __init__(self, stocknum):
+        self.stocknum = stocknum
+        m_drink.drink_num += 1
+
+    def __del__(self):
+        m_drink.drink_num -= 1
+
+#-------------------------------------------------------------------------------
+class n_drink:
+    drink_num = 0
+
+    def __init__(self, stocknum):
+        self.stocknum = stocknum
+        n_drink.drink_num += 1
+
+    def __del__(self):
+        n_drink.drink_num -= 1
+
+#-------------------------------------------------------------------------------
+class o_drink:
+    drink_num = 0
+
+    def __init__(self, stocknum):
+        self.stocknum = stocknum
+        o_drink.drink_num += 1
+
+    def __del__(self):
+        o_drink.drink_num -= 1
+
+#-------------------------------------------------------------------------------
+class p_drink:
+    drink_num = 0
+
+    def __init__(self, stocknum):
+        self.stocknum = stocknum
+        p_drink.drink_num += 1
+
+    def __del__(self):
+        p_drink.drink_num -= 1
+
+#-------------------------------------------------------------------------------
+class q_drink:
+    drink_num = 0
+
+    def __init__(self, stocknum):
+        self.stocknum = stocknum
+        q_drink.drink_num += 1
+
+    def __del__(self):
+        q_drink.drink_num -= 1
+
+#-------------------------------------------------------------------------------
+class r_drink:
+    drink_num = 0
+
+    def __init__(self, stocknum):
+        self.stocknum = stocknum
+        r_drink.drink_num += 1
+
+    def __del__(self):
+        r_drink.drink_num -= 1
+
+#-------------------------------------------------------------------------------
+class s_drink:
+    drink_num = 0
+
+    def __init__(self, stocknum):
+        self.stocknum = stocknum
+        s_drink.drink_num += 1
+
+    def __del__(self):
+        s_drink.drink_num -= 1
+
+#-------------------------------------------------------------------------------
+class t_drink:
+    drink_num = 0
+
+    def __init__(self, stocknum):
+        self.stocknum = stocknum
+        t_drink.drink_num += 1
+
+    def __del__(self):
+        t_drink.drink_num -= 1
+#-------------------------------------------------------------------------------
--- VendingMachine/main.py
+++ VendingMachine/main.py
@@ -1,15 +1,28 @@
-# Python Vendingmachine
+# Python Vending Machine
 
-print('Wellcome Kim\'s Vending Machine')
+machine_swich = 'Yes' # 기계 구동 스위치 값을 'Yes' 값으로 선언
 
-def First_Setting(Product_number, **Product_data):
+while True: # 무한 반복문 설정
 
-    print('Please input your Vending Machine data')
+    if machine_swich == 'Yes': # 기계 구동 스위치 값이 'Yes' 일 시에 프로그램 다시 시작
+        user_money = int(input('5000원 이하의 돈을 입력하여 주십시오.')) # 유저는 항상 큰 지폐 단위부터 돈을 입력한다 가정
 
-    Product_number = int(input('Enter product number in your vending machine'))
+        if user_money > 5000: # 만약 5000원 이상의 돈을 입력했을 시
+            print('5000원 이하의 돈을 입력하여 주시길 바랍니다.')
+            user_money = 0 # 투입 한 돈 값을 0으로 재설정
 
-    for i range(0,Product_number):
-        Product_data = input()
+        else:
+            # 초기 셋팅한 음료수 딕셔너리 출력 {name:cost} 형태
+
+            while True: # 무한 반복문 설정
+                user_drink = input('원하는 음료를 입력해 주시길 바랍니다.')
+
+                if 음료딕셔너리.get(user_drink) == 'None': # 음료 딕셔너리 안에 음료가 존재하지 않을 때
+                    print('{}는 자판기에 존재하지 않는 상품입니다. 다시 선택해주세요.'.format(user_drink))
+
+                else:
+                    print('{}를 선택하셨습니다.'.format(음료 딕셔너리.get(user_drink)))
 
 
-User_vaendingmachine = input('Enter, You want a product')
+    else: # 기계 구동 스위치 값이 'Yes'가 아니므로 잔돈 출력 후 반복문을 종료
+        break
